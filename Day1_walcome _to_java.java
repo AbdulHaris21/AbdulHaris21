06968b979513200e458e6b757e14164f04d8f914
AbdulHaris21 <99778916+AbdulHaris21@users.noreply.github.com>
2022-03-04 13:07:21 +0800

    Add files via upload

---
+++ Day1_walcome _to_java.java
@@ -0,0 +1,8 @@
+public class Day1_Welcome_To_Java {
+     public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+    
+}
