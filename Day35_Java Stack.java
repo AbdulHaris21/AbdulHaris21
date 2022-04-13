b535f9695e9004dc572699ccd1359c4be48fe615
AbdulHaris21 <99778916+AbdulHaris21@users.noreply.github.com>
2022-04-13 12:48:04 +0800

    Add files via upload

---
+++ Day35_Java Stack.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day35_Java_Stack {
+    public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            //Complete the code
+            while(input.length() != (input = input.replaceAll("\\(\\)|\\[\\]|\\{\\}","")).length());
+            System.out.println(input.isEmpty());
+		}
+		
+	}
+}
